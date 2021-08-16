a5fb7198f5bf0314c33cc2727343e88ff58d2c67
sumaiyapiastrisha <somaiyatrisha9290@gmail.com>
2021-08-16 23:18:38 +0600

    last

--- banking.js
+++ banking.js
@@ -1,29 +1,40 @@
 // deposite_function------------------------
+function adinput(input) {
 
-const depositeButton = document.getElementById('deposite_button');
-
-
-depositeButton.addEventListener('click', function () {
-
-    const addAmount = document.getElementById('deposite');
+    const addAmount = document.getElementById(input);
     const addAmount1 = addAmount.value;
     const addAmount2 = parseFloat(addAmount1);
-    console.log(addAmount2);
 
-    const current = document.getElementById('currentAmount');
+    return addAmount2;
+
+}
+// total function---------------------------
+function totalvalue(inputamount, addAmount2) {
+    const current = document.getElementById(inputamount);
     const current1 = current.innerText;
     const current2 = parseFloat(current1);
-    console.log(current2);
+
 
     const total = current2 + addAmount2;
+    return total;
+
+}
+
+// depositmethod---------------------------------
+
+const depositeButton = document.getElementById('deposite_button');
+depositeButton.addEventListener('click', function () {
+
+    const addAmount2 = adinput('deposite')
+    const total = totalvalue('currentAmount', addAmount2)
+
     currentAmount.innerText = total;
     deposite.value = '';
 
-    // balance------------------
     const balance1 = document.getElementById('balance');
     const balance2 = balance1.innerText;
     const balance3 = parseFloat(balance2);
-    console.log(balance3);
+
     const total_balance = balance3 + addAmount2;
     balance.innerText = total_balance;
 })
@@ -31,22 +42,22 @@ depositeButton.addEventListener('click', function () {
 // withdraw-------------------------------------------------
 const withdrawButton = document.getElementById('withdraw_button');
 withdrawButton.addEventListener('click', function () {
-    const withdraw1 = document.getElementById('withdraw');
-    const withdraw2 = withdraw1.value;
-    const withdraw3 = parseFloat(withdraw2);
-    withdraw_button.value = '';
 
-    const pwithdraw1 = document.getElementById('withdrawAmmount');
-    const pwithdraw2 = pwithdraw1.innerText;
-    const pwithdraw3 = parseFloat(pwithdraw2);
-    const totalwidthdraw = withdraw3 + pwithdraw3;
-    withdrawAmmount.innerText = totalwidthdraw;
+
+
+    const withdraw3 = adinput('withdraw')
+    const total = totalvalue('withdrawAmmount', withdraw3)
+    withdrawAmmount.innerText = total;
+    withdraw.value = '';
+
+
 
     const balance1 = document.getElementById('balance');
     const balance2 = balance1.innerText;
     const balance3 = parseFloat(balance2);
-    console.log(balance3);
+
     const total_balance = balance3 - withdraw3;
     balance.innerText = total_balance;
+
 
 })
